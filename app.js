93efe175a8bba1d7aeebb58988355ac96ef90488
Ere <ay139608@gmail.com>
2026-07-03 07:18:03 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+const app = express();
+const port = 3000;
+
+
+
+
+// middle ware 
+
+app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
+console.log("URL:", process.env.SUPABASE_URL);
+console.log("KEY:", process.env.SUPABASE_ANON_KEY);
+
+app.use('/api/views', require("./src/routes/page.js"));
+app.use("/za3amah", (req,res)=> {
+    res.render("landpage")
+})
+
+
+app.listen(port, () => {
+    console.log("it is started localhost:3000")
+})
